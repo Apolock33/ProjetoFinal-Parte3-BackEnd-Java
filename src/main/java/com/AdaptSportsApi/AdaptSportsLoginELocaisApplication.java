abf7cbefa5432a4efc9385fbb7cf74ace75e1adb
Apolock33 <carlosammgomes@gmail.com>
2022-03-22 21:11:19 -0300

    Deploy Tentativa 4

--- src/main/java/com/AdaptSportsApi/AdaptSportsLoginELocaisApplication.java
+++ src/main/java/com/AdaptSportsApi/AdaptSportsLoginELocaisApplication.java
@@ -2,12 +2,20 @@ package com.AdaptSportsApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableWebMvc
 @SpringBootApplication
-public class AdaptSportsLoginELocaisApplication {
+public class AdaptSportsLoginELocaisApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdaptSportsLoginELocaisApplication.class, args);
+	}
+	
+	public void addCorsMapping(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET","POST","PUT","DELETE");
 	}
 
 }
